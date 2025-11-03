f0952708b4b2f664165d092f22e6ff410fc070b8
rayanod2005 <rayanouladdaoud@gmail.com>
2025-11-03 16:13:10 +0100

    h

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/', function (req, res, next) {
     console.log('Middleware executed');
     next();
 }, serverRoutes);
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
 
